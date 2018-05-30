058e32f0745ae6f75c7f50bf81f53c79aff27c37
agpiermarini <piermarini.andrew@gmail.com>
2018-05-30 11:44:27 -0600

    Add test for oxford service

--- spec/services/oxford_service_spec.rb
+++ spec/services/oxford_service_spec.rb
@@ -5,4 +5,23 @@ describe OxfordService do
   it 'initializes with word' do
     expect(subject).to be_a OxfordService
   end
+
+  describe 'class methods' do
+    describe '#status' do
+      it 'returns status' do
+        VCR.use_cassette('oxford-service-status') do
+          expect(subject.status).to eq(200)
+          expect(subject.word_info).to be_a Hash
+        end
+      end
+    end
+
+    describe '#word_info' do
+      it 'returns hash with word information' do
+        VCR.use_cassette('oxford-service-word-info') do
+          expect(subject.word_info).to be_a Hash
+        end
+      end
+    end
+  end
 end
