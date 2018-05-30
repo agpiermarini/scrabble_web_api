3081242c947f3080ec454807ca73e6a9dbd5a716
agpiermarini <piermarini.andrew@gmail.com>
2018-05-30 11:33:41 -0600

    Move connection and response to private methods in word_search poro

--- app/search/word_search.rb
+++ app/search/word_search.rb
@@ -4,14 +4,6 @@ class WordSearch
   end
 
   def word
-    connection = Faraday.new "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{word_input}"
-
-    response = connection.get do | req |
-      req.headers["app_id"]  = ENV["app_id"]
-      req.headers["app_key"] = ENV["app_key"]
-    end
-
-
     if response.status == 200
       word_info = JSON.parse(response.body, symbolize_names: true)
       Word.new(return_word(word_info[:results][0]), return_inflection(word_info[:results][0]))
@@ -30,5 +22,16 @@ class WordSearch
 
     def return_inflection(result)
       result[:lexicalEntries][0][:inflectionOf][0][:id]
+    end
+
+    def connection
+      Faraday.new "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{word_input}"
+    end
+
+    def response
+      connection.get do | req |
+        req.headers["app_id"]  = ENV["app_id"]
+        req.headers["app_key"] = ENV["app_key"]
+      end
     end
 end
