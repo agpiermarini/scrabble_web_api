d8ce88848c4b39b2d4dba088073bbfa907ff3db3
agpiermarini <piermarini.andrew@gmail.com>
2018-05-30 11:30:39 -0600

    Add branch logic to plays controller to handle invalid word

--- app/controllers/api/v1/games/plays_controller.rb
+++ app/controllers/api/v1/games/plays_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Games::PlaysController < ActionController::API
   def create
-    require 'pry'; binding.pry
-    User.find(params[:user_id]).plays.create!(game_id: params[:id], word: params[:word])
-    render status: 201
+    word = WordSearch.new(params[:word]).word
+    if word.inflection
+      User.find(params[:user_id]).plays.create!(game_id: params[:id], word: params[:word])
+      render status: 201
+    else
+      message = {message: "#{params[:word]} is not a valid word."}
+      render json: message
+    end
   end
 end
--- spec/api/v1/requests/game_play_endpoint_post_spec.rb
+++ spec/api/v1/requests/game_play_endpoint_post_spec.rb
@@ -1,58 +1,58 @@
 require 'rails_helper'
 
 describe 'User' do
-  describe 'sends a post request to the game score endpoint' do
-      it 'returns a 201 created response' do
-      user_1 = create(:user)
-      user_2 = create(:user)
+  describe 'sends a post request to the game score endpoint with valid word' do
+    it 'returns a 201 created response' do
+      VCR.use_cassette('play-post-valid') do
+        user_1 = create(:user)
+        user_2 = create(:user)
 
-      game = create(:game, id: 1, player_1_id: user_1.id, player_2_id: user_2.id)
+        game = create(:game, id: 1, player_1_id: user_1.id, player_2_id: user_2.id)
 
-      user_1.plays.create!(game_id: game.id, word: "hello")
-      user_2.plays.create!(game_id: game.id, word: "hello")
+        user_1.plays.create!(game_id: game.id, word: "hello")
+        user_2.plays.create!(game_id: game.id, word: "hello")
 
-      post "/api/v1/games/1/plays?user_id=1&word=at"
+        post "/api/v1/games/1/plays?user_id=1&word=at"
 
-      expect(response.status).to be 201
+        expect(response.status).to be 201
 
-      get '/api/v1/games/1'
+        get '/api/v1/games/1'
 
-      info = JSON.parse(response.body, symbolize_names: true)
+        info = JSON.parse(response.body, symbolize_names: true)
 
-      expect(info).to be_a Hash
-      expect(info[:game_id]).to eq(game.id)
-      expect(info[:scores]).to be_an Array
-      expect(info[:scores].length).to eq(2)
+        expect(info).to be_a Hash
+        expect(info[:game_id]).to eq(game.id)
+        expect(info[:scores]).to be_an Array
+        expect(info[:scores].length).to eq(2)
 
-      expect(info[:scores][0]).to be_a Hash
-      expect(info[:scores][0][:user_id]).to eq(1)
-      expect(info[:scores][0][:score]).to eq(10)
+        expect(info[:scores][0]).to be_a Hash
+        expect(info[:scores][0][:user_id]).to eq(1)
+        expect(info[:scores][0][:score]).to eq(10)
 
-      expect(info[:scores][1]).to be_a Hash
-      expect(info[:scores][1][:user_id]).to eq(2)
-      expect(info[:scores][1][:score]).to eq(8)
+        expect(info[:scores][1]).to be_a Hash
+        expect(info[:scores][1][:user_id]).to eq(2)
+        expect(info[:scores][1][:score]).to eq(8)
+      end
+    end
+  end
+
+  describe 'sends a post request to the game score endpoint with invalid word' do
+    it 'returns a 201 created response' do
+      VCR.use_cassette('play-post-invalid') do
+        user_1 = create(:user)
+        user_2 = create(:user)
+
+        game = create(:game, id: 1, player_1_id: user_1.id, player_2_id: user_2.id)
+
+        user_1.plays.create!(game_id: game.id, word: "hello")
+        user_2.plays.create!(game_id: game.id, word: "hello")
+
+        post "/api/v1/games/1/plays?user_id=1&word=foxez"
+
+        info = JSON.parse(response.body, symbolize_names: true)
+
+        expect(info[:message]).to eq("foxez is not a valid word.")
+      end
     end
   end
 end
-# Background: This story assumes the base data from running `rake db:seed`
-#
-# You can choose to send the user_id and word specified below however you'd like or are comfortable.
-#
-# When I send a POST request to "/api/v1/games/1/plays" with a user_id=1 and word=at
-# Then I should receive a 201 Created Response
-#
-# When I send a GET request to "/api/v1/games/1" I receive a JSON response as follows:
-#
-# {
-#   "game_id":1,
-#   "scores": [
-#     {
-#       "user_id":1,
-#       "score":17
-#     },
-#     {
-#       "user_id":2,
-#       "score":16
-#     }
-#   ]
-# }
